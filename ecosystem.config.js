e0aa584b6c17332faa9c0d89338a442ad7d32db1
maxim <maximmalyshyn@gmail.com>
2023-12-17 20:31:37 +0200

    Initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    apps : [{
+        name   : "proxy-server",
+        script : "./src/index.js"
+    }]
+}
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import * as https from 'https';
+import proxy from 'express-http-proxy'
+import express from 'express'
+
+import {HttpsProxyAgent} from 'https-proxy-agent'
+import * as net from 'net'
+
+import * as http from 'http';
+import {Server} from "net";
+// console.log(net)
+
+const server = express();
+const PORT = 9999;
+
+// new http.Server(server)
+//     .listen(PORT, () => {
+//         console.log(`=== node version ${process.version}`)
+//         console.log(`=== server open weather stated on port ${PORT} ===`)
+//     });
+// server.
+
+
+export const proxyOpenRain = (req: any, res: any) => {
+     console.log(req.url)
+    const options = {
+        port: 80,
+        host: req.hostname,
+        path: req.path
+    };
+    const proxyRequest = http.request(options);
+    const chunks: Array<any> = [];
+    proxyRequest.on('response', function (proxyResponse) {
+        proxyResponse.on('data', function (chunk: Buffer) {
+            chunks.push(chunk)
+        });
+        proxyResponse.on('end', function () {
+            res.set(proxyResponse.headers)
+            res.send(Buffer.concat(chunks))
+        });
+    });
+    proxyRequest.on('error', function (err) {
+        console.error('proxyOpenRain history err3 ->', err);
+        res.send(err)
+    });
+    req.on('data', function (chunk) {
+        proxyRequest.write(chunk);
+    });
+    req.on('end', function () {
+        proxyRequest.end();
+    });
+};
+//  http://meteo-info.kiev.ua/#/radar
+// server.get('/', (req, res) => {
+//     console.log(req.host)
+//         res.send('Ololo')
+//     }
+// )
+ server.get('*', proxyOpenRain)
+
+server.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
+import './server-net'
